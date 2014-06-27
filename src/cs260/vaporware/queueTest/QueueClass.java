6e28c6e4b0c8999652c89ab8caa262eb33fe239b
ReverendCode <codenull@gmail.com>
2014-06-27 12:59:30 -0700

    Added arrayDouble filled out insertLeft and insertRight

--- src/cs260/vaporware/queueTest/QueueClass.java
+++ src/cs260/vaporware/queueTest/QueueClass.java
@@ -6,9 +6,9 @@ package cs260.vaporware.queueTest;
 public class QueueClass {
     private static final int DEFAULT_ARRAY = 100;
     private int[] array;
-    private int leftSide=0;
-    private int rightSide=0;
-    private int lastItem = DEFAULT_ARRAY;
+    private int leftSide=0;//these should point to the next available spot in the array
+    private int rightSide=1;
+    private int arraySize = DEFAULT_ARRAY;
 
 
     public QueueClass() {
@@ -16,13 +16,20 @@ public class QueueClass {
     }
     public QueueClass(int n){
         array = new int[n];
-        lastItem = n;
+        arraySize = n;
     }
-    public boolean insertLeft(int insert) {
-        return true;
+    public void insertLeft(int insert) {
+        if (isFull()) arrayDouble();
+        array[leftSide--] = insert;
+        if (leftSide<0) leftSide=arraySize-1;
+
+
     }
-    public boolean insertRight(int insert) {
-        return true;
+    public void insertRight(int insert) {
+        if (isFull()) arrayDouble();
+        array[rightSide++] = insert;
+        if (rightSide == arraySize) rightSide=0;
+
     }
     public int removeLeft() {
         return 0;
@@ -31,10 +38,25 @@ public class QueueClass {
         return 0;
     }
     public boolean isEmpty() {
+        if (leftSide+1==rightSide) return true;//if they are back to back, there is no space between them
         return false;
     }
     public boolean isFull() {
+        if (rightSide == leftSide) return true;//when the right passes the left, the array is full
         return false;
+    }
+
+    private void arrayDouble() {
+        int newArraySize = arraySize * 2;
+        int[] tempArray = new int[newArraySize];
+        for (int i=1;i<arraySize;i++) {
+            tempArray[i] = removeLeft();
+        }
+        array = tempArray;
+        leftSide=0;
+        rightSide=arraySize;
+        arraySize=newArraySize;
+
     }
 
 
